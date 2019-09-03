802031b7c61124f5c1d933a5874cf12a440a9808
nakanokosuke <jaj4fv9ec@icloud.com>
2019-09-03 20:23:13 +0900

    add-product

--- sample/src/router.js
+++ sample/src/router.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Product from "./views/Product.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -14,11 +16,13 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/product",
+      name: "product",
+      component: Product
     }
   ]
 });
